46c05a3eca3834278bd7014cb1a071b4f9e10151
42hm <came@kakao.com>
2022-03-23 03:30:51 +0900

    feat : order

--- refactoring/srcs/order_stack.c
+++ refactoring/srcs/order_stack.c
@@ -6,31 +6,49 @@
 /*   By: hmoon <hmoon@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/23 03:06:39 by hmoon             #+#    #+#             */
-/*   Updated: 2022/03/23 03:16:52 by hmoon            ###   ########.fr       */
+/*   Updated: 2022/03/23 03:30:39 by hmoon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/push_swap.h"
 
-void	swap(t_stack *a, t_stack *b, enum e_order set)
+void	swap(t_stack *a, t_stack *b)
 {
 	int	first;
 	int	second;
 
-	if (set == sa || set == ss)
-	{
-		first = pop_back(a);
-		second = pop_back(a);
-		push_back(a, first);
-		push_back(a, second);
-	}
-	if (set == sb || set == ss)
-	{
-		first = pop_back(b);
-		second = pop_back(b);
-		push_back(b, first);
-		push_back(b, second);
-	}
+	first = pop_back(a);
+	second = pop_back(a);
+	push_back(a, first);
+	push_back(a, second);
 }
 
+void	push(t_stack *src, t_stac *dest)
+{
+	int	num;
+
+	num = pop_back(src);
+	push_back(dest, num);
+}
+
+void	rotate(t_stack *stack)
+{
+	int	num;
+
+	num = pop_back(stack);
+	push_stack(stack, num);
+}
+
+void	reverse_rotate(t_stack *stack)
+{
+	int	num;
+
+	num = pop_stack(stack);
+	push_back(stack, num);
+}
+
+void	order(t_stack *a, t_stack *b, int print)
+{
+
+}
 
